cc7da6c106a039f7013e94204d8e8fafd1c34749
jesus <jesus@DESKTOP-08OCM78>
2021-10-01 14:31:09 +0100

    Adicionar arquivos de projeto.

---
+++ Formulário/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulário
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(DateTime.Now.Date.ToShortDateString());
+            listBox1.Items.Add(DateTime.Now.TimeOfDay.ToString());
+            listBox1.Items.Add(textBox3.Text);
+        }
+
+        private void textBox3_TextChanged   (object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox3.Clear();
+        }
+    }
+}
